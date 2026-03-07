889ff55a1b6d690b4b628d9a80e16e6847620cd1
Felix Amartey <amartey.n.l.felix@gmail.com>
2026-03-07 14:54:24 +0000

    completed

--- tests/setup.js
+++ tests/setup.js
@@ -1,3 +1,29 @@
-import app from "../src/app.js";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
 
-export default app;
+dotenv.config({ path: ".env.test" });
+process.env.JWT_SECRET;
+
+const mongoUri = process.env.MONGO_URI;
+
+beforeAll(async () => {
+	if (!mongoUri) {
+		throw new Error("MONGO_URI is not defined. Set it in .env.test (or .env).");
+	}
+
+	if (mongoose.connection.readyState === 0) {
+		await mongoose.connect(mongoUri, {
+			serverSelectionTimeoutMS: 10000
+		});
+	}
+
+	await mongoose.connection.dropDatabase();
+});
+
+afterAll(async () => {
+	if (mongoose.connection.readyState !== 0) {
+		await mongoose.connection.dropDatabase();
+	}
+
+	await mongoose.disconnect();
+});
